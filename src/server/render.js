d92e3d1cfc30717b275df4e994afd2436a56dca7
Balthazar Gronon <bgronon@gmail.com>
2018-11-01 15:58:41 +0100

    disable ssr

--- src/server/render.js
+++ src/server/render.js
@@ -1,19 +1,13 @@
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import { StaticRouter } from 'react-router'
 import { matchPath } from 'react-router-dom'
 
 import routes from '../routes'
 import createStore from '../store'
 import page from './page'
 
-import App from '../components/App'
-
 export default stats => async (req, res) => {
   try {
     const store = createStore()
 
-    const context = {}
     const promises = []
 
     routes.some(route => {
@@ -26,11 +20,7 @@ export default stats => async (req, res) => {
 
     await Promise.all(promises)
 
-    const routerProps = { location: req.url, context }
-    const root = <App store={store} Router={StaticRouter} routerProps={routerProps} />
-    const html = __DEV__ ? '' : renderToString(root)
-
-    res.end(page({ html, state: store.getState(), main: stats.main || 'bundle.js' }))
+    res.end(page({ html: '', state: store.getState(), main: stats.main || 'bundle.js' }))
   } catch (err) {
     res.status(500).send(err.stack)
   }
